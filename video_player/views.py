2bdbb4e05ed7a98070cc638fd8a4a05dddcfe9d0
wpotoke <146642624+wpotoke@users.noreply.github.com>
2024-06-16 00:12:36 +0300

    new function for streaming and rewind functional

--- video_player/views.py
+++ video_player/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 from movie_app.models import Movie, Serial
+from django.http import StreamingHttpResponse
+from .utils import open_file
+
 
 def film_player(request, slug_film):
     film = get_object_or_404(Movie, slug=slug_film)
-
     data = {"film": film}
-
-    return render(request, 'video_player/film_player.html', context=data)
+    return render(request, "video_player/film_player.html", context=data)
 
 
 def serial_player(request, slug_serial):
@@ -14,4 +15,14 @@ def serial_player(request, slug_serial):
 
     data = {"serial": serial}
 
-    return render(request, '', context=data)
+    return render(request, "", context=data)
+
+def stream_video(request, slug_film, quality):
+    file, status_code, content_length, content_range = open_file(request, slug_film, quality)
+    response = StreamingHttpResponse(file, status=status_code)
+    response['Accept-Ranges'] = 'bytes'
+    response['Content-Length'] = str(content_length)
+    response['Content-Type'] = 'video/mp4'
+    if content_range:
+        response['Content-Range'] = content_range
+    return response
