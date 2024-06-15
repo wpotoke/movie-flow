19df0d7303609f09cb35574ee37602dd8b6bf33c
wpotoke <146642624+wpotoke@users.noreply.github.com>
2024-06-16 00:11:00 +0300

    added new url path for video-player film

--- video_player/urls.py
+++ video_player/urls.py
@@ -5,5 +5,7 @@ app_name = "video_player"
 
 urlpatterns = [
     path('film/<slug:slug_film>/watch/', views.film_player, name='film-player'),
+    path('stream/<slug:slug_film>/<str:quality>/', views.stream_video, name='stream_video'),
     path('serial/<slug:slug_serial>/watch/', views.serial_player, name='serial-player'),
+    # path('stream/<slug:slug_serial>/<str:quality>/', views.?, name='stream_serial'),
     ]
